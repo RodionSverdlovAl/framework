0f11d4087b80e41177550aee6e1adec685a72f75
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-11-24 15:04:35 +0300

    Add getInstance method

---
+++ Fw/Core/Application.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Fw\Core\Application;
+
+class Application
+{
+    private $pager = null; // будет объект класса
+    private static $instance = null;
+    private $template = null; //будет объект класса
+
+    private function __construct()
+    {
+
+    } // закрываем конструктор
+    private function __clone()
+    {
+
+    }
+
+    public static function getInstance() : Application
+    {
+        if(self::$instance === null){
+            self:: $instance = new self();
+        }
+        return self::$instance;
+    }
+}
